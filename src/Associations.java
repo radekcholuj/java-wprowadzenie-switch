fc61a1c1006e75a20d061e70448dd18eb1db67d5
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-18 22:39:55 +0100

    #3 skojarzenia kolorów

---
+++ src/Associations.java
@@ -0,0 +1,20 @@
+public class Associations {
+    public static void main(String[] args) {
+
+        char Associations = 'h';
+
+        switch (Associations) {
+            case 'c':
+                System.out.println("Pomidor");
+                break;
+            case 'z':
+                System.out.println("Trawa");
+                break;
+            case 'n':
+                System.out.println("Morze");
+                break;
+            default:
+                System.out.println("Brak skojarzeń");
+        }
+    }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+    public static void main(String[] args) {
+        double a = 5.2;
+        double b = 2;
+        char option = '*';
+
+        double result = 0;
+
+        switch(option){
+            case '+':
+                result = (a+b);
+                break;
+            case '-':
+                result = (a-b);
+                break;
+            case '*':
+                result = (a*b);
+                break;
+            case '/':
+                result = (a/b);
+                break;
+        }
+
+        System.out.println(result);
+    }
+}
